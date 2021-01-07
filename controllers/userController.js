681441c8b2780fa80ad07b04a927bb502f56ae00
Joshua Mba <angelicjoekeyz@gmail.com>
2021-01-07 15:16:41 +0100

    Fixed next()

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,8 +7,8 @@ const response = require ('../utils/response')
 exports.registerUser = catchWrapper (async (req, res, next) => {
   const inputValidation = await regValidation.validateAsync(req.body)
   //check if user exist
-  const checkUser = User.findOne({email: inputValidation.email})
-  if (checkUser) res.status(400).send({message: `A user with this email ${inputValidation.email} already exist. Kindly try again`})
+  const checkUser = await User.findOne({email: inputValidation.email})
+  if (checkUser) next(res.status(400).send({message: `A user with this email ${inputValidation.email} already exist. Kindly try again`}))
   
   const user = new User({...inputValidation})
   await user.save ({ new: true }, (err) => {
